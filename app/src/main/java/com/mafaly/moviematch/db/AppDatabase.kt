7cd35da42daa8ad30ecd16b475bc5b373b3e4bb9
Fanny Guillemont <fanny.gt10@gmail.com>
2024-01-23 00:52:55 +0100

    (fix) get db instance

--- app/src/main/java/com/mafaly/moviematch/db/AppDatabase.kt
+++ app/src/main/java/com/mafaly/moviematch/db/AppDatabase.kt
@@ -1,6 +1,8 @@
 package com.mafaly.moviematch.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import com.mafaly.moviematch.db.daos.GameDao
 import com.mafaly.moviematch.db.entities.GameEntity
@@ -11,4 +13,29 @@ import com.mafaly.moviematch.db.entities.GameEntity
 )
 abstract class AppDatabase : RoomDatabase() {
     abstract fun gameDao(): GameDao
+
+    companion object {
+        // Volatile garantit que la variable est toujours lue à partir de la mémoire principale
+        // ainsi que les modifications apportées par un thread sont visibles par tous les autres threads
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getInstance(context: Context): AppDatabase {
+            if (INSTANCE != null) {
+                return INSTANCE!!
+            }
+
+            // Synchronisation du bloc bloc pour éviter que plusieurs threads créent plusieurs instances simultanément
+            synchronized(this) {
+                if (INSTANCE == null) {
+                    INSTANCE = Room.databaseBuilder(
+                        context.applicationContext,
+                        AppDatabase::class.java,
+                        "movie_match_db"
+                    ).build()
+                }
+                return INSTANCE!!
+            }
+        }
+    }
 }
--- app/src/main/java/com/mafaly/moviematch/views/MainActivity.kt
+++ app/src/main/java/com/mafaly/moviematch/views/MainActivity.kt
@@ -3,27 +3,28 @@ package com.mafaly.moviematch.views
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.room.Room
+import androidx.lifecycle.lifecycleScope
 import com.mafaly.moviematch.db.AppDatabase
+import com.mafaly.moviematch.db.entities.GameEntity
 import com.mafaly.moviematchduel.R
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
-    companion object {
-        lateinit var database: AppDatabase
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        database = Room.databaseBuilder(
-            this,
-            AppDatabase::class.java,
-            "movie_match_db"
-        ).build()
-
         Intent(this, MovieSelection::class.java).also {
             startActivity(it)
+        }
+
+        val appDatabase = AppDatabase.getInstance(applicationContext)
+
+        // Test
+        lifecycleScope.launch(Dispatchers.IO) {
+            val game = GameEntity(0, "Partie numéro 1", "2024-01-23", 6, "La La Land")
+            appDatabase.gameDao().insertNewGame(game)
         }
     }
 }
