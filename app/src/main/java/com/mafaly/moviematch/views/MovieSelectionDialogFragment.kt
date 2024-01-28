6a135e62edd64c312bd7f8028fbdb97698cd3da4
Maram ZITOUNI <zitouni.maram20@gmail.com>
2024-01-28 20:03:18 +0100

    add image in selectionDialog

--- app/src/main/java/com/mafaly/moviematch/views/MovieSelectionDialogFragment.kt
+++ app/src/main/java/com/mafaly/moviematch/views/MovieSelectionDialogFragment.kt
@@ -7,7 +7,9 @@ import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
+import com.bumptech.glide.Glide
 import com.mafaly.moviematch.repo.MovieViewModel
+import com.mafaly.moviematchduel.BuildConfig
 import com.mafaly.moviematchduel.databinding.SelectMovieBinding
 class MovieSelectionDialogFragment() : DialogFragment() {
 
@@ -17,15 +19,18 @@ class MovieSelectionDialogFragment() : DialogFragment() {
     private val movieViewModel: MovieViewModel by activityViewModels()
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = SelectMovieBinding.inflate(inflater, container, false)
-        movieViewModel.selectedMovie.observe(viewLifecycleOwner, Observer { movieData ->
-            binding.textViewMovieTitle.text = movieData.title
-
-        })
-
         return binding.root
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        movieViewModel.selectedMovie.observe(viewLifecycleOwner, Observer { movieData ->
+            binding.textViewMovieTitle.text = movieData.title
+            Glide.with(this)
+                .load(BuildConfig.TMDB_IMAGE_URL + movieData.posterPath)
+                //.placeholder(R.drawable.placeholder) // Replace with your placeholder image
+                //.error(R.drawable.error_image) // Replace with your error image
+                .into(binding.imageMoviePoster)
+        })
 
         binding.buttonNo.setOnClickListener {
             dismiss()
