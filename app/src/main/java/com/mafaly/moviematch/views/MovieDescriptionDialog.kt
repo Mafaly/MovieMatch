b6ae09f68d78577042301322095e2252cd6add16
Maram ZITOUNI <zitouni.maram20@gmail.com>
2024-01-28 19:52:50 +0100

    load moviedescription image

--- app/src/main/java/com/mafaly/moviematch/views/MovieDescriptionDialog.kt
+++ app/src/main/java/com/mafaly/moviematch/views/MovieDescriptionDialog.kt
@@ -1,13 +1,17 @@
 package com.mafaly.moviematch.views
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.Observer
+import com.bumptech.glide.Glide
 import com.mafaly.moviematch.repo.MovieViewModel
+import com.mafaly.moviematchduel.BuildConfig
+import com.mafaly.moviematchduel.R
 import com.mafaly.moviematchduel.databinding.DialogMovieDescriptionBinding
 
 class MovieDescriptionDialog : DialogFragment() {
@@ -27,8 +31,14 @@ class MovieDescriptionDialog : DialogFragment() {
             // Use movieData to update UI
             binding.movieDescriptionTitle.text = movieData.title
             binding.releaseDate.text=movieData.year
-            //binding.movieDescription.text = movieData.description
+            Glide.with(this) // Context is the Fragment itself
+                .load(BuildConfig.TMDB_IMAGE_URL + movieData.posterPath) // Combine base URL with path
+                .placeholder(R.drawable.poster)
+                //.error(R.drawable.error_image)
+                .into(binding.moviePoster)
         })
+            //binding.movieDescription.text = movieData.description
+
     }
 }
 
