989ecb16dc2f2040c619e978979e4799956b3ee7
Fanny Guillemont <fanny.gt10@gmail.com>
2024-02-04 18:46:07 +0100

    (fix) movieEntity

--- app/src/main/java/com/mafaly/moviematch/db/entities/MovieEntity.kt
+++ app/src/main/java/com/mafaly/moviematch/db/entities/MovieEntity.kt
@@ -6,7 +6,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "movie_entity")
 class MovieEntity(
-    @PrimaryKey(autoGenerate = true) val id: Int,
+    @PrimaryKey val id: Int,
     @ColumnInfo(name = "movie_title") val movieTitle: String,
     @ColumnInfo(name = "movie_year") val movieYear: String,
     @ColumnInfo(name = "movie_genre") val movieGenre: List<Int>?,
